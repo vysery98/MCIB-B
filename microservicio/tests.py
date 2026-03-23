f1fa7ea77d179afdbbbe1435b7deef2491d16c30
vysery98 <luispsi98@gmail.com>
2026-03-23 01:33:21 -0500

    Ejemplo ejecución CURL -POST

--- microservicio/tests.py
+++ microservicio/tests.py
@@ -16,21 +16,44 @@ def request_sumar():
     # Definimos la URL del endpoint
     url = 'http://localhost:8080/api/sumar'
 
-    # Ejemplo de headers (comentados porque no son necesarios en este caso)
-    ''' headers = {
+    '''
+    # Ejemplo de headers
+    headers = {
          'Content-Type': 'application/json',
          'Authorization': 'Bearer token'
-     }'''
-
+    }
+    '''
+    
     # Datos que enviaremos en el cuerpo de la petición
     data = {
-        'a': 5,   # Debe ser entero, no string (corregido)
+        'a': 5,
         'b': 3
     }
-
+    
     # Solicitud POST enviando los datos en formato JSON
-    response = requests.post(url, json=data)
+    response = requests.post(url, json = data, headers = headers) # Se agrega para aceptar HEADERS
+    '''
+    EJEMPLO CURL:
+        curl -X POST http://localhost:8080/api/sumar -H "Content-Type: application/json" -d "{\"a\":5, \"b\":32}"
 
+        EXPLICACIÓN PASO A PASO:
+            - -X POST
+                → Indica que el método HTTP es POST (por defecto sería GET).
+            - http://localhost:8080/api/sumar
+                → URL completa del endpoint al que se envía la solicitud.
+                "/api/sumar" = ruta definida en Flask.
+            - -H "Content-Type: application/json"
+                → Header HTTP que especifica que el cuerpo de la petición está en formato JSON.
+            - -d "{\"a\":5, \"b\":32}"
+                → Datos enviados en el cuerpo de la petición.
+                Aquí se envía un JSON con dos claves: a=5 y b=32.
+                Las barras invertidas (\") son para escapar las comillas dentro del comando.
+    
+        RESULTADO ESPERADO:
+            El servidor Flask recibe la petición POST en /api/sumar, procesa los datos y devuelve un JSON
+            con el resultado de la suma
+    '''
+    
     print(response) # Devuelve Status 
 
     # Retornamos la respuesta en formato JSON
